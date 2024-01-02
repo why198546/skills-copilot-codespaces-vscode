e2706bfc280e59d249e9e693fe7dece6cd1e50ab
why198546 <why198546@gmail.com>
2024-01-02 02:57:36 +0000

    Compilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsmember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    if (member.style.display === "none") {
+        member.style.display = "block";
+        skills.style.display = "none";
+    } else {
+        member.style.display = "none";
+        skills.style.display = "block";
+    }
+}
